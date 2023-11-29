bebde59f7c034eabe2b1787ff29af3843ed9b2a5
rustamovaj23 <tu7qc8kga@code.edu.az>
2023-11-29 12:02:27 +0400

    react-router-dom added

--- cafena-website/src/App.jsx
+++ cafena-website/src/App.jsx
@@ -1,10 +1,24 @@
-import './App.scss'
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.scss';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from './layout/MainLayout';
+import Home from './pages/Home';
 
 function App() {
 
   return (
     <>
-     
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="blogs" element={<Blogs />} /> */}
+          {/* <Route path="contact" element={<Contact />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
